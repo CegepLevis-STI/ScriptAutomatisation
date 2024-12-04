08c9d2f76f1d1020a3dcc95a2b97a9decf719c45
NicolasLemelinCLL <nicolas.lemelin@cegeplevis.ca>
2024-12-04 14:15:13 -0500

    MOdif encore

--- TestOcto.ps1
+++ TestOcto.ps1
@@ -1,2 +1,8 @@
-Write-Host Prenom: $args[0] Nom: $args[1]
-Read-Host -Prompt "Press any key to continue"
+param(
+    $Prenom = $args[0],
+    $Nom = $args[1],
+    $NumeroEmp = $args[2],
+    $Poste = $args[3]
+)
+
+Write-Host Prenom: $Prenom Nom: $Nom Numero: $Numero Poste: $Poste
