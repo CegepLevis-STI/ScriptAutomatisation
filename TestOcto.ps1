1ade48995ce3a36e3c0d595458fb6b459ca79914
NicolasLemelinCLL <nicolas.lemelin@cegeplevis.ca>
2024-12-06 07:18:12 -0500

    Modif test

--- TestOcto.ps1
+++ TestOcto.ps1
@@ -10,3 +10,20 @@ param(
 
 $OutputTest = "Prenom: " + $Prenom + " Nom: " + $Nom + " Numero: " + $NumeroEmp + " Departement: " + $Departement + " Poste: " + $Poste + "."
 Read-Host $OutputTest
+
+$ListDept = 'Département de physique et géologie', 'Approvisionnement', 'Service du cheminement et de l''organisation scolaires'
+$ListPoste_Approvisionnement = 'Acheteur/Acheteuse', 'Agent/Agente', 'Coordonnateur/Coordonatrice'
+
+foreach ($Dept in $ListDept) {
+    if ($Departement -eq $Dept)
+    {
+        foreach ($PosteDept in (Get-Variable -Name "ListPoste_$Dept").Value)
+        {
+            if ($Poste -eq $Poste)
+            {
+                Write-Host "Le poste de la personne est $PosteDept et elle fait partie du déparement $Dept"
+                Read-Host "Enter : "
+            }
+        }
+    }
+}
